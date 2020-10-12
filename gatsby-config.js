2bfc9f676f7bfac6823e202a7409f6687051bcba
Filipe <filipe@Filipe-MacBook.local>
2020-10-12 18:56:46 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Meu Blog`,
+    description: `A Blog about frontend and backend development`,
+    author: `@filipe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,14 @@
 import React from "react"
+import {PageProps} from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage:React.FC<PageProps>= () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: Não encontrado</h1>
+    <p>Ops... Não tem nada aqui</p>
   </Layout>
 )
 
