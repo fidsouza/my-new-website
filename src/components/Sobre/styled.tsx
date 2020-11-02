1de96355b4f9502b272bc8abae2c5e399b0e29d1
Filipe <filipe@Filipe-MacBook.local>
2020-11-02 20:57:53 -0300

    feature:caseSensitive

--- src/components/Sobre/styled.tsx
+++ src/components/Sobre/styled.tsx
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const SectionWrapper = styled.section`
 
   margin: auto;
   max-width: 70rem;
   padding: 2rem 5rem;
+
+  ${media.lessThan("large")`
+  
+     padding:1.7rem;
+  
+  `}
 
 
 `
@@ -16,11 +23,25 @@ export const Text = styled.p`
     color:var(--texts);
     line-height:1.95rem;
 
+    ${media.lessThan("large")`
+    
+    
+     font-size:1.2rem;
+    
+    `}
+
 `
 
 export const Title = styled.h1`
     font-size:2.5rem;
     color:var(--texts);
     line-height:6.5rem;
+    ${media.lessThan("large")`
+    
+      font-size:1.5rem;
+      line-height:4.5rem;
+
+    
+    `}
 
 `
